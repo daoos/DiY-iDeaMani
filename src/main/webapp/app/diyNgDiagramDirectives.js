6f19d2678ea11fd6520acb98fe70e38eb7f0c8db
MALLESH [Durga] NAGOTHI <mallesh-csra@users.noreply.github.com>
2018-05-04 00:52:23 -0400

    context menu feature introduced
    context menu feature introduced

--- src/main/webapp/app/diyNgDiagramDirectives.js
+++ src/main/webapp/app/diyNgDiagramDirectives.js
@@ -10,7 +10,115 @@ require(['angular'],function(){
         }
    })
     
-    /** Log Image **/
+   .directive('ideaManiProcessDiagram', ['$compile','$injector','$document', function($compile, $injector,$document) {
+        document.oncontextmenu = function(e) {
+          if(e.target.hasAttribute('right-click')){
+              return false;
+          }  
+        };
+        return {
+           restrict: 'EA',
+           transclude: false,
+           scope: {
+               nodesData: '=',
+               edgesData: '=',
+               graphOptions: '=',
+               callBackService: '='
+            },
+            link: function (scope, element, attr){
+                
+                var nodesData               = scope.nodesData;
+                var edgesData               = scope.edgesData;
+                var graphOptions            = scope.graphOptions;
+                var networkContainerWidth   = element[0].offsetHeight;
+                var networkContainerHeight  = element[0].offsetWidth;
+                var contextPopupMenu        = undefined;
+                var ctxPopupOpen            = false;
+                
+                /** injecting call back service **/
+                scope.callBackService    = attr.callBackService;
+                var proxyService         = $injector.get(scope.callBackService);
+                
+                /** badway of injecting vis**/
+                var vis                  = require("vis");
+                
+                var diagramData = {
+                    nodes: nodesData,
+                    edges: edgesData
+                };
+                
+                var processDiagram = new vis.Network(element[0], diagramData, graphOptions);
+                positionDigram();
+                
+                function positionDigram() {
+                    var nodeXYCoordinates = processDiagram.getPositions();
+                    console.log("nodeXYCoordinates ::",nodeXYCoordinates);
+                    console.log("networkContainerWidth ::",networkContainerWidth);
+                    console.log("networkContainerHeight ::",networkContainerHeight);
+                    var newX = networkContainerWidth / 4;
+                    var newY = networkContainerHeight / 4;
+                    //for now manually position it
+                    processDiagram.moveNode(1,-700,-400);
+                }
+                
+                processDiagram.on('click', function(params){ 
+                    console.log("params ::",params);
+                    destroyContextMenu();
+                });
+            
+                function destroyContextMenu(){
+                    if(ctxPopupOpen){
+                         var ctxPopupMenuElem = angular.element($document[0].querySelector('.popupContextMenu'));
+                         ctxPopupMenuElem.remove();
+                         ctxPopupOpen = false;
+                    }
+                }                      
+                
+                element.bind('contextmenu',function(e){
+                    console.log("User Right is hijaked ....")
+                    var pageX = e.pageX;
+                    var pageY = e.pageY;
+                    var offsetX = e.offsetX;
+                    var offsetY = e.offsetY;
+                    console.log("pageX ::",pageX);
+                    console.log("pageY ::",pageY);
+                    console.log("offsetX ::",offsetX);
+                    console.log("offsetY ::",offsetY);
+                    
+                    var contextMenuLeft  = (pageX - offsetX);
+                    var contextMenuTop  = (pageY - offsetY);
+                    console.log("contextMenuLeft ::",contextMenuLeft);
+                    console.log("contextMenuTop ::",contextMenuTop);
+                    if(ctxPopupOpen){
+                        destroyContextMenu();    
+                    }
+                    var selectedNodes = processDiagram.getSelection().nodes;
+                    //there are/is selected Nodes
+                    if(selectedNodes && selectedNodes.length >0){
+                        angular.forEach(selectedNodes, function(value, key) {
+                          console.log('Node key ' + key + ': '+ 'Node Value ' + + value);
+                            //later write the logic based on selected node type also based on multiple selection
+                        });
+                        var newElement = $compile('<div class="popupContextMenu" style="left:'+contextMenuLeft+'px; top:'+contextMenuTop+'px; "><div class="popupHeader">DiYRules-Context Menu</div><div class="ctxContent"><ul class="dashed-list"><li>+&nbsp;&nbsp;Add Stop Process</li><li>+&nbsp;&nbsp;Add Rules</li><li>+&nbsp;&nbsp;Add Rulesheet</li><li>+&nbsp;&nbsp;Add Process</li><li></li><li>-&nbsp;&nbsp;Delete Selected</li><li>+&nbsp;&nbsp;Add Connector</li></ul></div></div>')(scope);
+                          element.append(newElement);
+                          ctxPopupOpen = true;    
+                    }else{
+                        var newElement = $compile('<div class="popupContextMenu" style="left:'+contextMenuLeft+'px; top:'+contextMenuTop+'px; "><div class="popupHeader">DiYRules-Context Menu</div><div class="ctxContent"><ul class="dashed-list"><li>+&nbsp;&nbsp;Add Stop Process</li><li>+&nbsp;&nbsp;Add Rules</li><li>+&nbsp;&nbsp;Add Rulesheet</li><li>+&nbsp;&nbsp;Add Process</li><li></li><li>-&nbsp;&nbsp;Clear Canvas</li><li>&nbsp;&nbsp;&nbsp;Refresh</li></ul></div></div>')(scope);
+                        element.append(newElement);
+                        ctxPopupOpen = true;
+                    }
+                        
+                    
+                    e.preventDefault();
+                }); /** end of right click **/
+                
+            }    
+        };
+   
+   }])
+    
+    
+    /** diyProcessDiagram **/
    .directive('diyProcessDiagram', ['$compile','$injector','$document', function($compile, $injector,$document) {
        return { 
            restrict: 'EA',
@@ -26,12 +134,16 @@ require(['angular'],function(){
                console.log("options received::",scope.options);
                console.log("data received::",scope.data);
                
-               scope.callBackService = attr.callBackService;
-               var proxyService = $injector.get(scope.callBackService);
-               
-               
+               scope.callBackService    = attr.callBackService;
+               var proxyService         = $injector.get(scope.callBackService);
                //very bad way of injecting
-               var vis = require("vis");
+               var vis                  = require("vis");
+               var nodesData            = scope.data.nodes;
+               var nodes                = new vis.DataSet(nodesData);
+               console.log("nodesData :::::::::::::::::::::::::::::::",nodesData);
+               console.log("nodes    ::::::::::::::::::::::::::::::::",nodes);
+               
+               
                var processDiagramEvents = [
                    'click',
                    'doubleclick',
@@ -75,11 +187,12 @@ require(['angular'],function(){
                
                processDiagram = new vis.Network(element[0], scope.data, scope.options);
                
-               
                processDiagram.on('doubleClick', scope.onDoubleClick);
                processDiagram.on('hold', scope.onHoldClick);  
                processDiagram.on('release', scope.onReleaseClick);   
-               processDiagram.on('click', scope.onClick);   
+               processDiagram.on('click', function(params){
+                   scope.onClick(params);
+               });   
                
                
                
@@ -99,7 +212,23 @@ require(['angular'],function(){
                     console.log("execute onReleaseClick function@",onReleaseTime);
                }
                
-               scope.onClick = function() {
+               scope.onClick = function(params) {
+                   console.log("params ::", params);
+                   var nodeId = params['nodes']['0'];
+                   if(nodeId){
+                       console.log("clicked nodeId  ::",nodeId); 
+                       console.log("nodesData getAll()::",nodes.getIds());
+                       var clickedNode =  nodes.get(1);
+                       clickedNode = {id: 100, label: 'Mallesh'};
+                       clickedNode.color = {
+                           background: '#000000'
+                       }
+                       scope.data.nodes.update(clickedNode);
+                       
+                       console.log("clickedNode ::", clickedNode);
+                       
+                   }
+                   
                    console.log("execute onClick function@",processDiagram.getSelection());
                    var selectedEdges = processDiagram.getSelection().edges;
                    var selectedNodes = processDiagram.getSelection().nodes;
@@ -146,7 +275,10 @@ require(['angular'],function(){
                        processDiagram.on('doubleClick', scope.onDoubleClick);
                        processDiagram.on('hold', scope.onHoldClick);  
                        processDiagram.on('release', scope.onReleaseClick);
-                    processDiagram.on('click', scope.onClick);   
+                       processDiagram.on('click', function(params){
+                           console.log("scope.onClick ::",params);
+                           scope.onClick(params);
+                       }); 
                 });
                
                 scope.$watchCollection(function(){ return proxyService.visCanvasData}, function(processNodesData){
@@ -158,7 +290,10 @@ require(['angular'],function(){
                    processDiagram.on('doubleClick', scope.onDoubleClick);
                    processDiagram.on('hold', scope.onHoldClick);    
                    processDiagram.on('release', scope.onReleaseClick);
-                   processDiagram.on('click', scope.onClick);   
+                   processDiagram.on('click', function(params){
+                       console.log("scope.onClick ::",params);
+                       scope.onClick(params);
+                    });  
                     
                 });
                
--- src/main/webapp/app/home/homeControllers.js
+++ src/main/webapp/app/home/homeControllers.js
@@ -10,6 +10,90 @@ define([
     function homeController($rootScope,$scope,HomeFactory) {
         
         var mySelf = this;
+        
+        mySelf.nodesData = [];
+        mySelf.edgesData = [];
+        mySelf.nodesData.push({id: 1, "label": "Start", group: "edgenodes"});
+        mySelf.graphOptions = {
+            clickToUse: false,
+            interaction: {
+                dragNodes:true,
+                dragView: true,
+                hideEdgesOnDrag: false,
+                hideNodesOnDrag: false,
+                hover: true,
+                hoverConnectedEdges: true,
+                keyboard: {
+                  enabled: true,
+                  speed: {x: 10, y: 10, zoom: 0.02},
+                  bindToWindow: true
+                },
+                multiselect: true,
+                navigationButtons: true,
+                selectable: true,
+                selectConnectedEdges: true,
+                tooltipDelay: 30,
+                zoomView: true  
+            },
+            nodes:{
+                borderWidth: 1,
+                shadow: true,
+            },
+            groups: {
+                'edgenodes': {
+                    shape: 'dot',
+                    color: '#5daf73' 
+                },
+                'processnodes': {
+                    shape: 'box',
+                    color: '#3f3fa8' 
+                },
+                'rulesnodes': {
+                    shape: 'square',
+                    color: '#914072' 
+                },
+                'rulesheetnodes': {
+                    shape: 'square',
+                    color: '#914072' 
+                },
+                'decisionnodes': {
+                    shape: 'diamond',
+                    color: '#9fbe1c' 
+                },
+                
+            },
+            manipulation: {
+                enabled: false,
+                initiallyActive: true,
+                addNode: true,
+                addEdge: true,
+                editEdge: true,
+                deleteNode: true,
+                deleteEdge: true,
+                controlNodeStyle:{
+                  // all node options are valid.
+                }
+              },  
+            edges:{
+                arrows: {
+                  to:     {enabled: true, scaleFactor:1, type:'arrow'},
+                  middle: {enabled: false, scaleFactor:1, type:'arrow'},
+                  from:   {enabled: false, scaleFactor:1, type:'arrow'}
+                },
+                arrowStrikethrough: false,
+                chosen: true,
+                color: {
+                  color:'#1d1c1c',
+                  highlight:'#848484',
+                  hover: '#848484',
+                  inherit: 'from',
+                  opacity:1.0
+                }
+              },
+              physics: {stabilization: false}  
+        }
+        
+        
         mySelf.diyCanvasItems = {};
         
         /** Retrieving Canbas Tab Pills **/
